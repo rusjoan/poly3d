c9624b75e75941fd575ebccf7bbff4f4a3c39e2b
Evgeny Murashkin <evgeny@murdev.com>
2015-12-05 13:02:06 +0300

    Init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.base')
+
+@section('content')
+
+    <div class="row">
+          <div class="col-xs-12">
+            <div class="jumbotron">
+                <h1>Hello, world!</h1>
+                <p>...</p>
+                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('wrapper')
+    <div class="container">
+      
+
+              @yield('content')
+
+          </div>
+      </div>
+    </div>
+@endsection
